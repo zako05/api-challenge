3fac95380a4b60cde5459c6e6d353a31bef3c8b4
Michal Zak <zako05@gmail.com>
2021-05-31 16:08:13 +0200

    add 'todos' specs

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -1,9 +1,21 @@
 describe('/todo', () => {
-  // before(() => {
-  //   cy.request('POST', '/challenger').as('challenger')
-  // })
+  let todoZero
 
-  beforeEach(()=>{
+  before(() => {
+    // cy.request('POST', '/challenger').as('challenger')
+    cy.request({
+      method: 'GET',
+      url: '/todos',
+      headers: {
+        'X-CHALLENGER': '08ad2f98-9b2a-4cc0-b7cc-be97135c35af',
+      },
+    }).then((response) => {
+      todoZero = response.body.todos[0].id
+      return todoZero
+    })
+  })
+
+  beforeEach(() => {
     cy.visit('/gui/challenges/08ad2f98-9b2a-4cc0-b7cc-be97135c35af')
   })
 
@@ -13,12 +25,26 @@ describe('/todo', () => {
       url: '/todo',
       failOnStatusCode: false,
       headers: {
-        'X-CHALLENGER': '08ad2f98-9b2a-4cc0-b7cc-be97135c35af'
-      }
-    }).then(response => {
+        'X-CHALLENGER': '08ad2f98-9b2a-4cc0-b7cc-be97135c35af',
+      },
+    }).then((response) => {
       expect(response.status).to.eq(404)
     })
-    cy.get('body > div > table:nth-child(12) > tbody > tr:nth-child(2) > td:nth-child(3)').should('have.text', 'true')
+    cy.get(
+      'body > div > table:nth-child(12) > tbody > tr:nth-child(2) > td:nth-child(3)'
+    ).should('have.text', 'true')
   })
-  
+
+  it('GET /todo/id (404)', () => {
+    cy.request({
+      method: 'GET',
+      url: `/todo/${todoZero}`,
+      failOnStatusCode: false,
+      headers: {
+        'X-CHALLENGER': '08ad2f98-9b2a-4cc0-b7cc-be97135c35af',
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(404)
+    })
+  })
 })
--- cypress/integration/todos.spec.js
+++ cypress/integration/todos.spec.js
@@ -1,21 +1,60 @@
 describe('/todo', () => {
-  // before(() => {
-  //   cy.request('POST', '/challenger').as('challenger')
-  // })
+  const todoNotExist = 1000
+  let todoZero
 
-  // beforeEach(()=>{
-  //   cy.visit('/gui/challenges/08ad2f98-9b2a-4cc0-b7cc-be97135c35af')
-  // })
+  before(() => {
+    //   cy.request('POST', '/challenger').as('challenger')
+    cy.request({
+      method: 'GET',
+      url: '/todos',
+      headers: {
+        'X-CHALLENGER': '08ad2f98-9b2a-4cc0-b7cc-be97135c35af',
+      },
+    }).then((response) => {
+      todoZero = response.body.todos[0].id
+      return todoZero
+    })
+  })
+
+  beforeEach(()=>{
+    cy.visit('/gui/challenges/08ad2f98-9b2a-4cc0-b7cc-be97135c35af')
+  })
 
   it('GET /todos (200)', () => {
     cy.request({
       method: 'GET',
       url: '/todos',
       headers: {
-        'X-CHALLENGER': '08ad2f98-9b2a-4cc0-b7cc-be97135c35af'
-      }
-    }).then(response => {
+        'X-CHALLENGER': '08ad2f98-9b2a-4cc0-b7cc-be97135c35af',
+      },
+    }).then((response) => {
       expect(response.status).to.eq(200)
+    })
+  })
+
+  it('GET /todos/id (200)', () => {
+    cy.request({
+      method: 'GET',
+      url: `/todos/${todoZero}`,
+      failOnStatusCode: false,
+      headers: {
+        'X-CHALLENGER': '08ad2f98-9b2a-4cc0-b7cc-be97135c35af',
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200)
+    })
+  })
+
+  it('GET /todo/id (404)', () => {
+    cy.request({
+      method: 'GET',
+      url: `/todos/${todoNotExist}`,
+      failOnStatusCode: false,
+      headers: {
+        'X-CHALLENGER': '08ad2f98-9b2a-4cc0-b7cc-be97135c35af',
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(404)
     })
   })
 })
